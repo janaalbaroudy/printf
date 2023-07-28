f282825f5d1607a531caea10f985b6d9451a238b
Youmppy <headforesch00l@gmail.com>
2023-07-28 13:36:49 -0700

    ss

--- main.h
+++ main.h
@@ -1,18 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <unistd.h>
 #include <stdarg.h>
 
 /* checker for functions then print the string */
-void format_checker(char c, va_list ap); 
+void format_checker(char c, va_list ap);
 
 /* Function to print a single character */
 int _putchar(char c);
+
 /* Function to print a string */
-void str_print(const char *s); 
+void str_print(const char *s);
 
 /* Function to convert an integer to a string */
 void d_i(int d);
+
 /* Function to print an integer in binary format */
 void b_print(int x);
 
